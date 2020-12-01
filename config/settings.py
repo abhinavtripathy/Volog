004e8157e6914b844f802057fda36f5943c7a27b
abhinavtripathy <abhinavtripathy99@gmail.com>
2020-12-01 17:08:17 -0500

    minor

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = '+c@4(^^l+%mm0x!8yskbg@01zwdv7v*m_96ym$m*^+usvjd4j_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 # Application definition
 
--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 python3 manage.py vologo 
-python3 manage.py runserver
+python3 manage.py runserver 0.0.0.0:80
